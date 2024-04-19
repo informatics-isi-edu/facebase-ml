be6aeb0b53493999f8a977d6505524a7b0eee215
Zhiweiii <lizhiwei@usc.edu>
2024-04-18 18:16:45 -0700

    Change FaceBaseML __init__ to facebase config.

--- src/facebase_ml.py
+++ src/facebase_ml.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from PIL import Image
 from deriva_ml.deriva_ml_base import DerivaML, DerivaMLException
 from pathlib import Path, PurePath
+
+
 # import re
 
 
@@ -15,7 +17,7 @@ class FaceBaseMLException(DerivaMLException):
 
 class FaceBaseML(DerivaML):
     """
-    FaceBaseML is a class that extends DerivaML and provides additional routines for working with eye-ai
+    FaceBaseML is a class that extends DerivaML and provides additional routines for working with Facebase
     catalogs using deriva-py.
 
     Attributes:
@@ -27,32 +29,16 @@ class FaceBaseML(DerivaML):
     - pb (PathBuilder): The PathBuilder object for constructing URL paths.
 
     Methods:
-    - __init__(self, hostname: str = 'www.eye-ai.org', catalog_number: str = 'eye-ai'): Initializes the EyeAI object.
-    - create_new_vocab(self, schema_name: str, table_name: str, name: str, description: str, synonyms: List[str] = [],
-            exist_ok: bool = False) -> str: Creates a new controlled vocabulary in the catalog.
-    - image_tall(self, dataset_rid: str, diagnosis_tag_rid: str): Retrieves tall-format image data based on provided
-      diagnosis tag filters.
-    - add_process(self, process_name: str, github_url: str = "", process_tag: str = "", description: str = "",
-                    github_checksum: str = "", exists_ok: bool = False) -> str: Adds a new process to the Process table.
-    - compute_diagnosis(self, df: pd.DataFrame, diag_func: Callable, cdr_func: Callable,
-                          image_quality_func: Callable) -> List[dict]: Computes new diagnosis based on
-                                                                       provided functions.
-    - insert_new_diagnosis(self, entities: List[dict[str, dict]], diagTag_RID: str, process_rid: str): Batch inserts new
-      diagnosis entities into the Diagnoisis table.
-
-    Private Methods:
-    - _find_latest_observation(df: pd.DataFrame): Finds the latest observations for each subject in the DataFrame.
-    - _batch_insert(table: datapath._TableWrapper, entities: Sequence[dict[str, str]]): Batch inserts
-       entities into a table.
+    - __init__(self, hostname: str = 'ml.facebase.org', catalog_number: str = 'eye-ai'): Initializes the EyeAI object.
     """
 
-    def __init__(self, hostname: str = 'www.eye-ai.org', catalog_id: str = 'eye-ai', data_dir: str= './'):
+    def __init__(self, hostname: str = 'ml.facebase.org', catalog_id: str = 'isa', data_dir: str = './'):
         """
-        Initializes the EyeAI object.
+        Initializes the FacebaseML object.
 
         Args:
         - hostname (str): The hostname of the server where the catalog is located.
         - catalog_number (str): The catalog number or name.
         """
 
-        super().__init__(hostname, catalog_id, 'eye-ai', data_dir)
+        super().__init__(hostname, catalog_id, 'isa', data_dir)
